b959a663ae3185dbcebff334c15e169d6f60d54c
Menno Holtkamp <holtkamp@users.noreply.github.com>
2017-07-01 11:39:11 +0200

    Added CLI argument and option

--- src/Command/RunOctopusCommand.php
+++ src/Command/RunOctopusCommand.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Octopus\Command;
 
-use Octopus\Config as OctopusConfig;
+use Octopus\Config;
 use Octopus\Processor as OctopusProcessor;
 use Octopus\TargetManager as OctopusTargetManager;
 use Symfony\Component\Console\Command\Command;
@@ -13,25 +13,37 @@ use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RunOctopusCommand extends Command
 {
     private const SITEMAP_FILE = 'sitemap';
+    private const CONCURRENCY = 'concurrency';
 
     protected function configure(): void
     {
         $this
             ->setName('octopus:run')
             ->setDescription('Run the Octopus Sitemap Crawler.')
-            ->addArgument(self::SITEMAP_FILE, InputArgument::REQUIRED, 'What is the location of the sitemap you want to crawl?');
+            ->addArgument(self::SITEMAP_FILE, InputArgument::REQUIRED, 'What is the location of the sitemap you want to crawl?')
+            ->addOption(self::CONCURRENCY, null, InputOption::VALUE_OPTIONAL, 'The amount of connections used concurrently')
+            ->setHelp(
+                sprintf('
+Usage:
+<info> - php application.php %1$s http://www.domain.ext/sitemap.xml</info>
+using a specific concurrency:
+<info> - php application.php %1$s http://www.domain.ext/sitemap.xml --%2$s 15</info>',
+                    $this->getName(),
+                    self::CONCURRENCY
+                )
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Starting Octopus Sitemap Crawler');
-        $config = new OctopusConfig();
-        $config->targetFile = $input->getArgument(self::SITEMAP_FILE);
+        $config = $this->determineConfiguration($input);
         $targetManager = new OctopusTargetManager($config);
         $processor = new OctopusProcessor($config, $targetManager);
 
@@ -63,6 +75,18 @@ class RunOctopusCommand extends Command
                 implode(PHP_EOL, $content)
             );
         }
+    }
+
+    private function determineConfiguration(InputInterface $input): Config
+    {
+        $config = new Config();
+        if (is_numeric($input->getOption(self::CONCURRENCY))) {
+            $config->concurrency = (int)$input->getOption(self::CONCURRENCY);
+        }
+
+        $config->targetFile = $input->getArgument(self::SITEMAP_FILE);
+
+        return $config;
     }
 
     private function renderResultsTable(OutputInterface $output, OctopusProcessor $processor): void
