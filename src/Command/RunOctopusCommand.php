031e6af1f72ad4c37cf90ff3b4c01e9a53c7a5fc
Menno Holtkamp <holtkamp@users.noreply.github.com>
2017-10-03 17:23:18 +0200

    Improve and extend logging information (#8)
    * Add duration information
    
    * Wrap running Processor in separate method
    
    * Improve logging
    
    * Remove unneeded newlines

--- src/Command/RunOctopusCommand.php
+++ src/Command/RunOctopusCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Octopus\Command;
 
+use DateTime;
 use Octopus\Config;
 use Octopus\Processor as OctopusProcessor;
 use Octopus\TargetManager as OctopusTargetManager;
@@ -18,34 +19,98 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RunOctopusCommand extends Command
 {
-    private const SITEMAP_FILE = 'sitemap';
-    private const CONCURRENCY = 'concurrency';
+    /**
+     * @var string
+     */
+    private const COMMAND_ARGUMENT_SITEMAP_FILE = 'sitemap';
+
+    /**
+     * @var string
+     */
+    private const COMMAND_OPTION_CONCURRENCY = 'concurrency';
+
+    /**
+     * @var string
+     */
+    private const COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT = 'additionalResponseHeadersToCount';
+
+    /**
+     * @var string
+     */
+    private const DATE_FORMAT = DateTime::W3C;
+
+    /**
+     * @var DateTime
+     */
+    private $crawlingStartedDateTime;
+
+    /**
+     * @var DateTime
+     */
+    private $crawlingEndedDateTime;
 
     protected function configure(): void
     {
         $this
             ->setName('octopus:run')
             ->setDescription('Run the Octopus Sitemap Crawler.')
-            ->addArgument(self::SITEMAP_FILE, InputArgument::REQUIRED, 'What is the location of the sitemap you want to crawl?')
-            ->addOption(self::CONCURRENCY, null, InputOption::VALUE_OPTIONAL, 'The amount of connections used concurrently')
+            ->addArgument(self::COMMAND_ARGUMENT_SITEMAP_FILE, InputArgument::REQUIRED, 'What is the location of the sitemap you want to crawl?')
+            ->addOption(self::COMMAND_OPTION_CONCURRENCY, null, InputOption::VALUE_OPTIONAL, 'The amount of connections used concurrently')
+            ->addOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT, null, InputOption::VALUE_OPTIONAL, 'A comma separated list of the additional response headers to keep track of / count during crawling')
             ->setHelp(
-                sprintf('Usage:
+                sprintf(
+                    'Usage:
 <info> - php application.php %1$s http://www.domain.ext/sitemap.xml</info>
 using a specific concurrency:
 <info> - php application.php %1$s http://www.domain.ext/sitemap.xml --%2$s 15</info>',
                     $this->getName(),
-                    self::CONCURRENCY
+                    self::COMMAND_OPTION_CONCURRENCY
                 )
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->crawlingStartedDateTime = new DateTime();
         $output->writeln('Starting Octopus Sitemap Crawler');
-        $config = $this->determineConfiguration($input);
+
+        $config = $this->determineConfiguration($input, $output);
         $targetManager = new OctopusTargetManager($config);
         $processor = new OctopusProcessor($config, $targetManager);
 
+        $this->runProcessor($processor, $targetManager, $output);
+
+        $this->crawlingEndedDateTime = new DateTime();
+
+        $output->writeln(str_repeat(PHP_EOL, 2));
+        $this->renderResultsTable($output, $processor);
+
+        if ($config->outputBroken && count($processor->brokenUrls) > 0) {
+            $this->outputBrokenUrls($processor, $output, $config->outputDestination);
+        }
+    }
+
+    private function determineConfiguration(InputInterface $input, OutputInterface $output): Config
+    {
+        $config = new Config();
+        $config->targetFile = $input->getArgument(self::COMMAND_ARGUMENT_SITEMAP_FILE);
+        $output->writeln('Loading URLs from Sitemap: ' . $config->targetFile);
+
+        if (is_numeric($input->getOption(self::COMMAND_OPTION_CONCURRENCY))) {
+            $config->concurrency = (int)$input->getOption(self::COMMAND_OPTION_CONCURRENCY);
+            $output->writeln('Using concurrency: ' . $config->concurrency);
+        }
+        if(is_string($input->getOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT))){
+            $additionalResponseHeadersToCount = $input->getOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT);
+            $config->additionalResponseHeadersToCount = explode(',', $additionalResponseHeadersToCount);
+            $output->writeln('Keep track of additional response headers: ' . $additionalResponseHeadersToCount);
+        }
+
+        return $config;
+    }
+
+    private function runProcessor(OctopusProcessor $processor, OctopusTargetManager $targetManager, OutputInterface $output): void
+    {
         try {
             $numberOfQueuedFiles = $targetManager->populate();
             $output->writeln($numberOfQueuedFiles . ' URLs queued for crawling');
@@ -59,53 +124,64 @@ using a specific concurrency:
         while ($targetManager->countQueue()) {
             $processor->run();
         }
-
-        $output->writeln(str_repeat(PHP_EOL, 2));
-        $this->renderResultsTable($output, $processor);
-
-        if ($config->outputBroken && count($processor->brokenUrls) > 0) {
-            $content = array();
-            foreach ($processor->brokenUrls as $url => $httpStatusCode) {
-                $label = sprintf('Failed %d: %s', $httpStatusCode, $url);
-                $output->writeln($label);
-                $content[] = $label;
-            }
-            file_put_contents(
-                $config->outputDestination . '/broken.txt',
-                implode(PHP_EOL, $content)
-            );
-        }
-    }
-
-    private function determineConfiguration(InputInterface $input): Config
-    {
-        $config = new Config();
-        $config->targetFile = $input->getArgument(self::SITEMAP_FILE);
-        if (is_numeric($input->getOption(self::CONCURRENCY))) {
-            $config->concurrency = (int)$input->getOption(self::CONCURRENCY);
-        }
-
-        return $config;
     }
 
     private function renderResultsTable(OutputInterface $output, OctopusProcessor $processor): void
     {
+        $rowColumnSpan = array('colspan' => count($processor->statCodes));
+
         $table = new Table($output);
         $table->setHeaders(
             array(
-                array(new TableCell('Crawling summary for: ' . $processor->config->targetFile, array('colspan' => count($processor->statCodes)))),
+                array(new TableCell('Crawling summary for: ' . $processor->config->targetFile, $rowColumnSpan)),
                 array_keys($processor->statCodes),
             )
         );
         $table->addRow(array_values($processor->statCodes));
         $table->addRow(new TableSeparator());
+
         $table->addRows(
             array(
-                array(new TableCell('Applied concurrency: ' . $processor->config->concurrency, array('colspan' => count($processor->statCodes)))),
-                array(new TableCell('Total amount of processed data: ' . $processor->totalData, array('colspan' => count($processor->statCodes)))),
-                array(new TableCell('Failed to load #URLs: ' . count($processor->brokenUrls), array('colspan' => count($processor->statCodes)))),
+                array(new TableCell('Crawling started: ' . $this->getCrawlingStartedLabel(), $rowColumnSpan)),
+                array(new TableCell('Crawling ended: ' . $this->getCrawlingEndedLabel(), $rowColumnSpan)),
+                array(new TableCell('Crawling duration: ' . $this->getCrawlingDurationLabel(), $rowColumnSpan)),
+                array(new TableCell('Applied concurrency: ' . $processor->config->concurrency, $rowColumnSpan)),
+                array(new TableCell('Total amount of processed data: ' . $processor->totalData, $rowColumnSpan)),
+                array(new TableCell('Failed to load #URLs: ' . count($processor->brokenUrls), $rowColumnSpan)),
             )
         );
         $table->render();
+    }
+
+    private function getCrawlingStartedLabel(): string
+    {
+        return $this->crawlingStartedDateTime->format(self::DATE_FORMAT);
+    }
+
+    private function getCrawlingEndedLabel(): string
+    {
+        return $this->crawlingEndedDateTime->format(self::DATE_FORMAT);
+    }
+
+    private function getCrawlingDurationLabel(): string
+    {
+        $numberOfSeconds = $this->crawlingEndedDateTime->getTimestamp() - $this->crawlingStartedDateTime->getTimestamp();
+
+        return sprintf('%d seconds', $numberOfSeconds);
+    }
+
+    private function outputBrokenUrls(OctopusProcessor $processor, OutputInterface $output, string $outputDestination): void
+    {
+        $content = array();
+        foreach ($processor->brokenUrls as $url => $httpStatusCode) {
+            $label = sprintf('Failed %d: %s', $httpStatusCode, $url);
+            $output->writeln($label);
+            $content[] = $label;
+        }
+
+        file_put_contents(
+            $outputDestination . '/broken.txt',
+            implode(PHP_EOL, $content)
+        );
     }
 }
