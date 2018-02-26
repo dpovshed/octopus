e9d30d06c761922ca2b3b6569e17ecf076ba1274
Menno Holtkamp <holtkamp@users.noreply.github.com>
2018-02-26 18:24:05 -0300

    Use PHP-CS fixer configuration file with extended configuration

--- src/Command/RunOctopusCommand.php
+++ src/Command/RunOctopusCommand.php
@@ -76,13 +76,13 @@ class RunOctopusCommand extends Command
             ->setDescription('Run the Octopus Sitemap Crawler.')
             ->addArgument(self::COMMAND_ARGUMENT_SITEMAP_FILE, InputArgument::REQUIRED, 'What is the location of the sitemap you want to crawl?')
             ->addOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT, null, InputOption::VALUE_OPTIONAL, 'A comma separated list of the additional response headers to keep track of / count during crawling')
-            ->addOption(self::COMMAND_OPTION_CONCURRENCY, null, InputOption::VALUE_OPTIONAL, 'The amount of connections used concurrently. Defaults to ' . Config::CONCURRENCY_DEFAULT)
-            ->addOption(self::COMMAND_OPTION_FOLLOW_HTTP_REDIRECTS, null, InputOption::VALUE_OPTIONAL, 'Should the crawler follow HTTP redirects? Defaults to ' . (Config::FOLLOW_HTTP_REDIRECTS_DEFAULT ? 'true' : 'false'))
-            ->addOption(self::COMMAND_OPTION_USER_AGENT, null, InputOption::VALUE_OPTIONAL, 'The UserAgent to use when issuing requests, defaults to ' . Config::REQUEST_HEADER_USER_AGENT_DEFAULT)
-            ->addOption(self::COMMAND_OPTION_REQUEST_TYPE, null, InputOption::VALUE_OPTIONAL, 'The type of HTTP request, HEAD put lesser load to server while GET loads whole page. Defaults to ' . Config::REQUEST_TYPE_DEFAULT)
-            ->addOption(self::COMMAND_OPTION_TIMEOUT, null, InputOption::VALUE_OPTIONAL, 'Timeout for a request, in seconds. Defaults to ' . Config::TIMEOUT_DEFAULT)
+            ->addOption(self::COMMAND_OPTION_CONCURRENCY, null, InputOption::VALUE_OPTIONAL, 'The amount of connections used concurrently. Defaults to '.Config::CONCURRENCY_DEFAULT)
+            ->addOption(self::COMMAND_OPTION_FOLLOW_HTTP_REDIRECTS, null, InputOption::VALUE_OPTIONAL, 'Should the crawler follow HTTP redirects? Defaults to '.(Config::FOLLOW_HTTP_REDIRECTS_DEFAULT ? 'true' : 'false'))
+            ->addOption(self::COMMAND_OPTION_USER_AGENT, null, InputOption::VALUE_OPTIONAL, 'The UserAgent to use when issuing requests, defaults to '.Config::REQUEST_HEADER_USER_AGENT_DEFAULT)
+            ->addOption(self::COMMAND_OPTION_REQUEST_TYPE, null, InputOption::VALUE_OPTIONAL, 'The type of HTTP request, HEAD put lesser load to server while GET loads whole page. Defaults to '.Config::REQUEST_TYPE_DEFAULT)
+            ->addOption(self::COMMAND_OPTION_TIMEOUT, null, InputOption::VALUE_OPTIONAL, 'Timeout for a request, in seconds. Defaults to '.Config::TIMEOUT_DEFAULT)
             ->setHelp(
-                sprintf(
+                \sprintf(
                     'Usage:
 <info> - php application.php %1$s http://www.domain.ext/sitemap.xml</info>
 using a specific concurrency:
@@ -106,10 +106,10 @@ using a specific concurrency:
 
         $this->crawlingEndedDateTime = new DateTime();
 
-        $output->writeln(str_repeat(PHP_EOL, 2));
+        $output->writeln(\str_repeat(PHP_EOL, 2));
         $this->renderResultsTable($output, $processor);
 
-        if ($config->outputBroken && count($processor->brokenUrls) > 0) {
+        if ($config->outputBroken && \count($processor->brokenUrls) > 0) {
             $this->outputBrokenUrls($processor, $output, $config->outputDestination);
         }
     }
@@ -118,34 +118,34 @@ using a specific concurrency:
     {
         $config = new Config();
         $config->targetFile = $input->getArgument(self::COMMAND_ARGUMENT_SITEMAP_FILE);
-        $output->writeln('Loading URLs from Sitemap: ' . $config->targetFile);
+        $output->writeln('Loading URLs from Sitemap: '.$config->targetFile);
 
-        if (is_string($input->getOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT))) {
+        if (\is_string($input->getOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT))) {
             $additionalResponseHeadersToCount = $input->getOption(self::COMMAND_OPTION_ADDITIONAL_RESPONSE_HEADERS_TO_COUNT);
-            $config->additionalResponseHeadersToCount = explode(',', $additionalResponseHeadersToCount);
-            $output->writeln('Keep track of additional response headers: ' . $additionalResponseHeadersToCount);
+            $config->additionalResponseHeadersToCount = \explode(',', $additionalResponseHeadersToCount);
+            $output->writeln('Keep track of additional response headers: '.$additionalResponseHeadersToCount);
         }
-        if (is_numeric($input->getOption(self::COMMAND_OPTION_CONCURRENCY))) {
-            $config->concurrency = (int)$input->getOption(self::COMMAND_OPTION_CONCURRENCY);
-            $output->writeln('Using concurrency: ' . $config->concurrency);
+        if (\is_numeric($input->getOption(self::COMMAND_OPTION_CONCURRENCY))) {
+            $config->concurrency = (int) $input->getOption(self::COMMAND_OPTION_CONCURRENCY);
+            $output->writeln('Using concurrency: '.$config->concurrency);
         }
-        if (is_string($input->getOption(self::COMMAND_OPTION_FOLLOW_HTTP_REDIRECTS))) {
+        if (\is_string($input->getOption(self::COMMAND_OPTION_FOLLOW_HTTP_REDIRECTS))) {
             $followRedirectsValue = $input->getOption(self::COMMAND_OPTION_FOLLOW_HTTP_REDIRECTS);
             $config->followRedirects = $followRedirectsValue === 'true' ? true : false;
-            $output->writeln('Follow HTTP redirects: ' . $followRedirectsValue);
+            $output->writeln('Follow HTTP redirects: '.$followRedirectsValue);
         }
-        if (is_string($input->getOption(self::COMMAND_OPTION_USER_AGENT))) {
+        if (\is_string($input->getOption(self::COMMAND_OPTION_USER_AGENT))) {
             $userAgentValue = $input->getOption(self::COMMAND_OPTION_USER_AGENT);
             $config->requestHeaders[$config::REQUEST_HEADER_USER_AGENT] = $userAgentValue;
-            $output->writeln('Use UserAgent for issued requests: ' . $userAgentValue);
+            $output->writeln('Use UserAgent for issued requests: '.$userAgentValue);
         }
-        if (is_string($input->getOption(self::COMMAND_OPTION_REQUEST_TYPE))) {
+        if (\is_string($input->getOption(self::COMMAND_OPTION_REQUEST_TYPE))) {
             $config->requestType = $input->getOption(self::COMMAND_OPTION_REQUEST_TYPE);
-            $output->writeln('Using request type: ' . $config->requestType);
+            $output->writeln('Using request type: '.$config->requestType);
         }
-        if (is_numeric($input->getOption(self::COMMAND_OPTION_TIMEOUT))) {
-            $config->timeout = (float)$input->getOption(self::COMMAND_OPTION_TIMEOUT);
-            $output->writeln('Using per-request timeout: ' . $config->timeout);
+        if (\is_numeric($input->getOption(self::COMMAND_OPTION_TIMEOUT))) {
+            $config->timeout = (float) $input->getOption(self::COMMAND_OPTION_TIMEOUT);
+            $output->writeln('Using per-request timeout: '.$config->timeout);
         }
 
         $config->validate();
@@ -157,11 +157,11 @@ using a specific concurrency:
     {
         try {
             $numberOfQueuedFiles = $targetManager->populate();
-            $output->writeln($numberOfQueuedFiles . ' URLs queued for crawling');
+            $output->writeln($numberOfQueuedFiles.' URLs queued for crawling');
             $processor->warmUp();
             $processor->spawnBundle();
-        } catch (\Exception $e) {
-            $output->writeln('Exception on initialization: ' . $e->getMessage());
+        } catch (\Exception $exception) {
+            $output->writeln('Exception on initialization: '.$exception->getMessage());
             exit;
         }
 
@@ -172,29 +172,29 @@ using a specific concurrency:
 
     private function renderResultsTable(OutputInterface $output, OctopusProcessor $processor): void
     {
-        ksort($processor->statCodes);
-        $rowColumnSpan = array('colspan' => count($processor->statCodes));
+        \ksort($processor->statCodes);
+        $rowColumnSpan = ['colspan' => \count($processor->statCodes)];
 
         $table = new Table($output);
         $table->setHeaders(
-            array(
-                array(new TableCell('Crawling summary for: ' . $processor->config->targetFile, $rowColumnSpan)),
-                array_keys($processor->statCodes),
-            )
+            [
+                [new TableCell('Crawling summary for: '.$processor->config->targetFile, $rowColumnSpan)],
+                \array_keys($processor->statCodes),
+            ]
         );
-        $table->addRow(array_values($processor->statCodes));
+        $table->addRow(\array_values($processor->statCodes));
         $table->addRow(new TableSeparator());
 
         $table->addRows(
-            array(
-                array(new TableCell('Crawling started: ' . $this->getCrawlingStartedLabel(), $rowColumnSpan)),
-                array(new TableCell('Crawling ended: ' . $this->getCrawlingEndedLabel(), $rowColumnSpan)),
-                array(new TableCell('Crawling duration: ' . $this->getCrawlingDurationLabel(), $rowColumnSpan)),
-                array(new TableCell('Applied concurrency: ' . $processor->config->concurrency, $rowColumnSpan)),
-                array(new TableCell('Total amount of processed data: ' . $processor->totalData, $rowColumnSpan)),
-                array(new TableCell('Failed to load #URLs: ' . count($processor->brokenUrls), $rowColumnSpan)),
-                array(new TableCell('Redirected #URLs: ' . count($processor->redirectedUrls), $rowColumnSpan)),
-            )
+            [
+                [new TableCell('Crawling started: '.$this->getCrawlingStartedLabel(), $rowColumnSpan)],
+                [new TableCell('Crawling ended: '.$this->getCrawlingEndedLabel(), $rowColumnSpan)],
+                [new TableCell('Crawling duration: '.$this->getCrawlingDurationLabel(), $rowColumnSpan)],
+                [new TableCell('Applied concurrency: '.$processor->config->concurrency, $rowColumnSpan)],
+                [new TableCell('Total amount of processed data: '.$processor->totalData, $rowColumnSpan)],
+                [new TableCell('Failed to load #URLs: '.\count($processor->brokenUrls), $rowColumnSpan)],
+                [new TableCell('Redirected #URLs: '.\count($processor->redirectedUrls), $rowColumnSpan)],
+            ]
         );
         $table->render();
     }
@@ -213,21 +213,21 @@ using a specific concurrency:
     {
         $numberOfSeconds = $this->crawlingEndedDateTime->getTimestamp() - $this->crawlingStartedDateTime->getTimestamp();
 
-        return sprintf('%d seconds', $numberOfSeconds);
+        return \sprintf('%d seconds', $numberOfSeconds);
     }
 
     private function outputBrokenUrls(OctopusProcessor $processor, OutputInterface $output, string $outputDestination): void
     {
-        $content = array();
+        $content = [];
         foreach ($processor->brokenUrls as $url => $reason) {
-            $label = sprintf('Failed %s: %s', $reason, $url);
+            $label = \sprintf('Failed %s: %s', $reason, $url);
             $output->writeln($label);
             $content[] = $label;
         }
 
-        file_put_contents(
-            $outputDestination . '/broken.txt',
-            implode(PHP_EOL, $content)
+        \file_put_contents(
+            $outputDestination.'/broken.txt',
+            \implode(PHP_EOL, $content)
         );
     }
 }
--- src/Processor.php
+++ src/Processor.php
@@ -12,14 +12,12 @@ use React\EventLoop\Factory as EventLoopFactory;
 use React\EventLoop\LibEventLoop;
 use React\EventLoop\Timer\Timer;
 use React\HttpClient\Client as HttpClient;
+use React\Promise\Timer\TimeoutException;
 use function React\Promise\race;
 use function React\Promise\Timer\reject;
-use React\Promise\Timer\TimeoutException;
 
 /**
  * Processor core.
- *
- * @package Octopus
  */
 class Processor
 {
@@ -29,7 +27,7 @@ class Processor
     public $statCodes = [];
 
     /**
-     * Total amount of processed data
+     * Total amount of processed data.
      *
      * @var int
      */
@@ -65,7 +63,7 @@ class Processor
     private $saveEnabled;
 
     /**
-     * to use with configuration elements
+     * to use with configuration elements.
      *
      * @var string
      */
@@ -109,9 +107,9 @@ class Processor
         $this->config = $config;
         $this->saveEnabled = $config->outputMode === 'save';
         if ($this->saveEnabled || $config->outputBroken) {
-            $this->savePath = $config->outputDestination . DIRECTORY_SEPARATOR;
-            if (!@mkdir($this->savePath) && !is_dir($this->savePath)) {
-                throw new Exception('Cannot create output directory: ' . $this->savePath);
+            $this->savePath = $config->outputDestination.DIRECTORY_SEPARATOR;
+            if (!@\mkdir($this->savePath) && !\is_dir($this->savePath)) {
+                throw new Exception('Cannot create output directory: '.$this->savePath);
             }
         }
     }
@@ -122,22 +120,22 @@ class Processor
         $countRunning = $this->targetManager->countRunning();
         $countFinished = $this->targetManager->countFinished();
 
-        $codeInfo = array();
+        $codeInfo = [];
         foreach ($this->statCodes as $code => $count) {
-            $codeInfo[] = sprintf('%s: %d', $code, $count);
+            $codeInfo[] = \sprintf('%s: %d', $code, $count);
         }
 
-        echo sprintf(
+        echo \sprintf(
             " %5.1fMB %6.2f sec. Queued/running/done: %d/%d/%d. Statistics: %s           \r",
-            memory_get_usage(true) / 1048576,
-            microtime(true) - $this->started,
+            \memory_get_usage(true) / 1048576,
+            \microtime(true) - $this->started,
             $countQueue,
             $countRunning,
             $countFinished,
-            implode(' ', $codeInfo)
+            \implode(' ', $codeInfo)
         );
 
-        if (0 === ($countQueue + $countRunning)) {
+        if (($countQueue + $countRunning) === 0) {
             $timer->cancel();
         }
     }
@@ -155,7 +153,7 @@ class Processor
 
     public function run(): void
     {
-        $this->loop->addPeriodicTimer($this->config->timerUI, array($this, 'timerStatistics'));
+        $this->loop->addPeriodicTimer($this->config->timerUI, [$this, 'timerStatistics']);
         $this->loop->addPeriodicTimer($this->config->timerQueue, function (Timer $timer) {
             if ($this->targetManager->hasFreeSlots()) {
                 $this->spawnBundle();
@@ -164,13 +162,13 @@ class Processor
             }
         });
 
-        $this->started = microtime(true);
+        $this->started = \microtime(true);
         $this->loop->run();
     }
 
     public function spawnBundle(): void
     {
-        for ($i = $this->targetManager->getFreeSlots(); $i > 0; $i--) {
+        for ($i = $this->targetManager->getFreeSlots(); $i > 0; --$i) {
             //list($id, $url) = $this->targets->launchAny(); //TODO make configurable to either launch the next, or a random URL
             list($id, $url) = $this->targetManager->launchNext();
             $this->spawn($id, $url);
@@ -180,24 +178,25 @@ class Processor
     private function spawn(int $id, string $url): void
     {
         $this->spawnWithBrowser($id, $url);
+
         return;
     }
 
     private function spawnWithBrowser(int $id, string $url): void
     {
-        $requestType = strtolower($this->config->requestType);
+        $requestType = \mb_strtolower($this->config->requestType);
 
         race([
                 reject($this->config->timeout, $this->loop),
-                $this->browser->$requestType($url, $this->config->requestHeaders)
+                $this->browser->$requestType($url, $this->config->requestHeaders),
             ]
         )->then(
             function (ResponseInterface $response) use ($id, $url) {
                 $this->countAdditionalHeaders($response->getHeaders());
 
                 if ($this->saveEnabled) {
-                    $path = $this->savePath . $this->makeFilename($url, $id);
-                    if (file_put_contents($path, $response->getBody(), FILE_APPEND) === false) {
+                    $path = $this->savePath.$this->makeFilename($url, $id);
+                    if (\file_put_contents($path, $response->getBody(), FILE_APPEND) === false) {
                         throw new Exception("Cannot write file: $path");
                     }
                 }
@@ -208,30 +207,31 @@ class Processor
                 $this->statCodes[$httpResponseCode] = isset($this->statCodes[$httpResponseCode]) ? $this->statCodes[$httpResponseCode] + 1 : 1;
                 $this->targetManager->done($id);
 
-                if ($this->config->followRedirects && in_array($httpResponseCode, $this->httpRedirectionResponseCodes, true)) {
+                if ($this->config->followRedirects && \in_array($httpResponseCode, $this->httpRedirectionResponseCodes, true)) {
                     $headers = $response->getHeaders();
                     $newLocation = $headers['Location'][0];
                     $this->redirectedUrls[$url] = $newLocation;
                     $this->targetManager->add($newLocation);
+
                     return;
                 }
 
                 // Any 2xx code is 'success' for us, if not => failure
-                if ((int)($httpResponseCode / 100) !== 2) {
+                if ((int) ($httpResponseCode / 100) !== 2) {
                     $this->brokenUrls[$url] = $httpResponseCode;
+
                     return;
                 }
 
-                if (random_int(0, 100) < $this->config->bonusRespawn) {
+                if (\random_int(0, 100) < $this->config->bonusRespawn) {
                     $this->targetManager->add($url);
                 }
             },
             function (Exception $exception) use ($id, $url) {
                 $failType = 'failed'; // Generic, cannot qualify with more details.
-                if (is_a($exception, TimeoutException::class)) {
+                if (\is_a($exception, TimeoutException::class)) {
                     $failType = 'timeouted';
-                }
-                elseif (is_a($exception, ResponseException::class) && $exception->getCode() >= 300) {
+                } elseif (\is_a($exception, ResponseException::class) && $exception->getCode() >= 300) {
                     // Regular HTTP error code.
                     $failType = $exception->getCode();
                 }
@@ -240,25 +240,25 @@ class Processor
                     // Init just to prevent warning.
                     $this->statCodes[$failType] = 0;
                 }
-                $this->statCodes[$failType]++;
+                ++$this->statCodes[$failType];
                 $this->targetManager->done($id);
                 $this->brokenUrls[$url] = $failType;
 
-                echo $url . ' request error: ' . $exception->getMessage() . PHP_EOL;
+                echo $url.' request error: '.$exception->getMessage().PHP_EOL;
             }
         );
 
         if ($this->config->spawnDelayMax) {
-            usleep(random_int($this->config->spawnDelayMin, $this->config->spawnDelayMax));
+            \usleep(\random_int($this->config->spawnDelayMin, $this->config->spawnDelayMax));
         }
     }
 
     private function countAdditionalHeaders(array $headers): void
     {
-        if (is_array($this->config->additionalResponseHeadersToCount) && count($this->config->additionalResponseHeadersToCount) > 0) {
+        if (\is_array($this->config->additionalResponseHeadersToCount) && \count($this->config->additionalResponseHeadersToCount) > 0) {
             foreach ($this->config->additionalResponseHeadersToCount as $additionalHeader) {
                 if (isset($headers[$additionalHeader])) {
-                    $headerLabel = sprintf('%s (%s)', $additionalHeader, $headers[$additionalHeader][0]);
+                    $headerLabel = \sprintf('%s (%s)', $additionalHeader, $headers[$additionalHeader][0]);
                     $this->statCodes[$headerLabel] = isset($this->statCodes[$headerLabel]) ? $this->statCodes[$headerLabel] + 1 : 1;
                 }
             }
@@ -267,7 +267,6 @@ class Processor
 
     private function makeFilename(string $octopusUrl, int $octopusId): string
     {
-        return preg_replace('/[^a-zA-Z0-9]/', '_', $octopusUrl . '_____' . $octopusId);
+        return \preg_replace('/[^a-zA-Z0-9]/', '_', $octopusUrl.'_____'.$octopusId);
     }
-
 }
